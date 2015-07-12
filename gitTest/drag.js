f5c8aba79a5bd392e5cdf6c7926822bb52c78fbf
wingcarl <wingcarlos5@gmail.com>
2015-07-12 11:31:20 +0800

    complete drag demo

--- gitTest/drag.js
+++ gitTest/drag.js
@@ -1,9 +1,18 @@
-//javascript Document
+﻿//javascript Document
 function drag(id){
 	var obj = document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
-	obj.onmousedown = function(){
-		
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
+		document.onmousemove = function(ev){
+			obj.style.left = ev.pageX - disX + 'px';
+			obj.style.top = ev.pageY - disY + 'px';
+		};
+		document.onmouseup = function(){
+			document.onmousemove = null;
+			document.onmouseup = null;
+		};
 	}
 }
