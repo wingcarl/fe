5f3ed8ff3f72a42d5a19470cdcff55b3b8b9c453
wingcarl <wingcarlos5@gmail.com>
2015-09-16 09:21:12 +0800

    tools.js

--- js/javascript-the-definitive-guide/客户端技术/tools.js
+++ js/javascript-the-definitive-guide/客户端技术/tools.js
@@ -9,3 +9,23 @@ function onLoad(f){
 
 onLoad.loaded = false;
 onLoad(function(){onLoad.loaded = true;})
+
+function getScrollOffsets(w) {
+	w = w || window;
+	if(w.pageXOffset != null )return {x:w.pageXOffset,y:w.pageYOffset};
+	var d = w.document;
+	if(document.compatMode == "CSS1Compat")
+		return {x:d.documentElement.scrollLeft,y:d.documentElement.scrollTop};
+
+	return {x:d.body.scrollLeft,y:d.body.scrollTop};
+}
+
+function getViewportSize(w){
+	w = w || window;
+	if(w.innerWidth != null) return {w:w.innerWidth,h:w.innerHeight};
+
+	var d = w.document;
+	if(document.compatMode == "CSS1Compat")
+		return {w:d.documentElement.clientWidth,h:w.documentElement.clientHeight};
+	return {w:d.body.clientWidth,h:d.body.clientHeight};
+}
