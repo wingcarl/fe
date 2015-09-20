97b1f50b1ac40dd9b478c47e43070aec8d2d3f3b
wingcarl <wingcarlos5@gmail.com>
2015-09-20 22:25:14 +0800

    manacher

---
+++ js/algorithm/kmp.js
@@ -0,0 +1,46 @@
+function caculateNext(arr){
+	//var i = 0;
+	var next = [-1];
+	var k = -1;
+	var j = 0;
+	while(j<arr.length){
+		if(k==-1 || arr[k]==arr[j]){
+			j++;
+			k++;
+			if(arr[j] == arr[k])
+				next[j] = next[k];
+			else
+				next[j] = k;
+		}else {
+			k = next[k]
+		}
+	}
+	return next;
+}
+
+function kmp(source,pattern){
+	var i = 0;
+	var ans = -1;
+	var j = 0;
+	var next = caculateNext(pattern);
+	while(i<source.length){
+		if(j==-1 || pattern[j]  == source[i]){
+			i++;
+			j++;
+		}else{
+			j = next[j];
+		}
+		if(j == pattern.length){
+			ans = i - pattern.length;
+			break;
+		}
+	}
+	return ans;
+}
+
+function consoleLog(){
+	var i = kmp("abcdaab",'daa');
+	var next = caculateNext('abababab');
+	console.log(next);
+	console.log(i);
+}
